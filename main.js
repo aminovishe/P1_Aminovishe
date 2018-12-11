7d695afb6d8a93638843de6102d87c77f83e5032
aminovishe <aminhad32@gmail.com>
2018-12-11 21:39:11 +0100

    ajouter dossier bootstrap

---
+++ main.js
@@ -0,0 +1,6 @@
+function EN_ver() {
+    document.getElementById('bienvenue').innerHTML = 'Welcome to Konnect';
+}
+function FR_ver() {
+    document.getElementById('bienvenue').innerHTML = 'Bienvenue chez Konnect';
+}
